9d51c09ab23dac45b1c3e3aefec70353c6c97dbb
Gabe <gabe1946402@gmail.com>
2024-08-19 21:49:08 -0300

    better

---
+++ OlaMundo.java
@@ -0,0 +1,8 @@
+public class OlaMundo {
+}
+public static void main(String[] arg) {
+
+    OlaMundo ola = new OlaMundo();
+    ola.imprimirDados();
+    ola.imprimir("Fulano","48");
+}
--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class OlaMundo {
+    String nome = "UDESC!";
+    int idade = 42;
+
+    void imprimirDados(){
+        System.out.printIn(nome + "-" + idade);
+
+
+    }
+    void imprimir(String nome, int idade) {
+
+        System.out.printIn(nome + "-" + idade);
+    }
+
 }
